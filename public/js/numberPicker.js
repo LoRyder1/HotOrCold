acd5eb130a596005c89cedb36a11060878e7980b
Lovinder Pnag <lspnag@gmail.com>
2014-10-02 20:56:38 -0500

    added gradient

--- public/js/numberPicker.js
+++ public/js/numberPicker.js
@@ -19,15 +19,42 @@ $(document).ready(function(){
 			location.reload();
 		}
 
-		if((value >= (computerNumber + 6)) || (value <= (computerNumber - 6))){
-			$(this).css("background", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
+		// if((value >= (computerNumber + 6)) || (value <= (computerNumber - 6))){
+		// 	console.log(value);
+		// 	console.log(computerNumber);
+		// 	var num = (computerNumber)-(value)+5
+		// 	console.log(num)
+		// 	$(this).css("background", "linear-gradient(to right, #1e5799 0%,blue " + num + "%, red 100%)");
+		// 	clickCount -= 1;
+		// 	$('.num_remaining').html(clickCount);
+		// } else if((value <= (computerNumber + 6)) || (value >= (computerNumber - 6))){
+		// 	var num = (computerNumber)- (value)+5
+		// 	$(this).css("background", "linear-gradient(to right, #1e5799 0%,blue 10%, red " + num + "%)");
+		// 	clickCount -= 1;
+		// 	$('.num_remaining').html(clickCount);
+		// }
+
+		if(value < computerNumber){
+			var num = (computerNumber-value);
+			console.log(computerNumber)
+
+			$(this).css("background", "linear-gradient(to right, #1e5799 0%,blue " + num + "%, red 100%)");
 			clickCount -= 1;
 			$('.num_remaining').html(clickCount);
-		} else if((value <= (computerNumber + 6)) || (value >= (computerNumber - 6))){
-			$(this).css("background-color", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
+
+		} else if(value > computerNumber){
+			var num = Math.abs((computerNumber-value));
+			console.log(computerNumber);
+
+			$(this).css("background", "linear-gradient(to right, #1e5799 0%,red " + num + "%, blue 100%)");
 			clickCount -= 1;
 			$('.num_remaining').html(clickCount);
 		}
+
+
+
+
+
 
 	});
 
